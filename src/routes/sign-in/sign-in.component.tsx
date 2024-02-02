cc7ffcb8828b91350d6975fdb9a619d0155d5756
Brady Peterson <bpeterson1663@gmail.com>
2024-02-02 06:26:31 -0600

    feat(auth): redirect google sign in

--- src/routes/sign-in/sign-in.component.tsx
+++ src/routes/sign-in/sign-in.component.tsx
@@ -1,6 +1,20 @@
-import { signInWithGooglePopUp, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils"
+import { useEffect } from 'react'
+import { getRedirectResult } from 'firebase/auth'
+import { auth, signInWithGooglePopUp, createUserDocumentFromAuth, signinWithGoogleRedirect } from "../../utils/firebase/firebase.utils"
 
 export default function SignIn() {
+    useEffect(() => {
+        const loadSignIn = async () => {
+            const response = await getRedirectResult(auth)
+            if (response) {
+                const user = await createUserDocumentFromAuth(response.user)
+                console.log({user})
+
+            }
+        }
+        loadSignIn()
+    }, [])
+
     const logGoogleUser = async () => {
         const response = await signInWithGooglePopUp()
         const user = await createUserDocumentFromAuth(response.user)
@@ -11,6 +25,7 @@ export default function SignIn() {
         <div>
         <h1>Sign In</h1>
         <button onClick={logGoogleUser}>Sign In with Google</button>
+        <button onClick={signinWithGoogleRedirect}>Sign In with Google Redirect</button>
         </div>
     )
 }
