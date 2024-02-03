39204b5061c56e3faf027fbe16432ba538701c40
Brady Peterson <bpeterson1663@gmail.com>
2024-02-03 06:29:26 -0600

    feat(auth): remove redirect page

--- src/routes/sign-in/sign-in.component.tsx
+++ src/routes/sign-in/sign-in.component.tsx
@@ -1,31 +1,16 @@
-import { useEffect } from 'react'
-import { getRedirectResult } from 'firebase/auth'
-import { auth, signInWithGooglePopUp, createUserDocumentFromAuth, signinWithGoogleRedirect } from "../../utils/firebase/firebase.utils"
+import { signInWithGooglePopUp, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils"
 
 export default function SignIn() {
-    useEffect(() => {
-        const loadSignIn = async () => {
-            const response = await getRedirectResult(auth)
-            if (response) {
-                const user = await createUserDocumentFromAuth(response.user)
-                console.log({user})
-
-            }
-        }
-        loadSignIn()
-    }, [])
 
     const logGoogleUser = async () => {
         const response = await signInWithGooglePopUp()
         const user = await createUserDocumentFromAuth(response.user)
-        console.log({response, user})
     }
 
     return (
         <div>
         <h1>Sign In</h1>
         <button onClick={logGoogleUser}>Sign In with Google</button>
-        <button onClick={signinWithGoogleRedirect}>Sign In with Google Redirect</button>
         </div>
     )
 }
--- src/utils/firebase/firebase.utils.ts
+++ src/utils/firebase/firebase.utils.ts
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app'
-import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider, type User } from 'firebase/auth'
+import { getAuth, signInWithPopup, GoogleAuthProvider, type User } from 'firebase/auth'
 import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore'
 
 const firebaseConfig = {
@@ -23,7 +23,6 @@ googleProvider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopUp = () => signInWithPopup(auth, googleProvider)
-export const signinWithGoogleRedirect = () => signInWithRedirect(auth, googleProvider)
 
 export const db = getFirestore()
 
